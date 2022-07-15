d9a9f9e353d9a070cd4a3ceac293382ec5f4774b
Archana Agrawal <archana.agrawal3@outlook.com>
2022-07-15 19:50:53 -0400

    added comments, deleted some duplicate code

--- src/express_server.js
+++ src/express_server.js
@@ -23,6 +23,8 @@ app.use(
 );
 app.use(express.urlencoded({ extended: true }));
 
+// home endpoints
+
 app.get("/", (req, res) => {
   const userId = req.session.user_id;
   if (!userId) {
@@ -31,13 +33,70 @@ app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+//Methods for user management endpoints
+
+app.get("/register", (req, res) => {
+  const userId = req.session.user_id;
+  if (userId && users[userId]) {
+    return res.redirect("/urls");
+  }
+  req.session = null;
+  const templateVar = { user: undefined };
+  res.render("urls_registration", templateVar);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.post("/register", (req, res) => {
+  const id = generateRandomString();
+  const email = req.body.email;
+  const password = req.body.password;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  if (!email || !password) {
+    res.status(400);
+    return res.send("Error:400; Invalid email or password");
+  }
+  if (getUserByEmail(users, email)) {
+    res.status(400);
+    return res.send("Error:400; user already exists");
+  }
+  users[id] = { id, email, hashedPassword };
+  req.session.user_id = id; // eslint-disable-line camelcase
+  res.redirect("/urls");
 });
+
+app.get("/login", (req, res) => {
+  const userId = req.session.user_id;
+  if (userId && users[userId]) {
+    return res.redirect("/urls");
+  }
+  req.session = null;
+  const templateVar = { user: undefined };
+  res.render("urls_login", templateVar);
+});
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!getUserByEmail(users, email)) {
+    res.status(403);
+    return res.send("Error: 403; email not found");
+  }
+  const id = getUserByEmail(users, email);
+  const userPassword = users[id].hashedPassword;
+  if (!bcrypt.compareSync(password, userPassword)) {
+    res.status(403);
+    return res.send("Error: 403; password does not match");
+  }
+  req.session.user_id = id; // eslint-disable-line camelcase
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  req.session = null;
+  res.redirect("/login");
+});
+
+// This endpoint gets the /urls page
 
 app.get("/urls", (req, res) => {
   const userId = req.session.user_id;
@@ -56,6 +115,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVar);
 });
 
+//This endpoint gets the new URL page
 app.get("/urls/new", (req, res) => {
   const userId = req.session.user_id;
   if (!userId || !users[userId]) {
@@ -67,26 +127,7 @@ app.get("/urls/new", (req, res) => {
   return res.render("urls_new", templateVar);
 });
 
-app.get("/register", (req, res) => {
-  const userId = req.session.user_id;
-  if (userId && users[userId]) {
-    return res.redirect("/urls");
-  }
-  req.session = null;
-  const templateVar = { user: undefined };
-  res.render("urls_registration", templateVar);
-});
-
-app.get("/login", (req, res) => {
-  const userId = req.session.user_id;
-  if (userId && users[userId]) {
-    return res.redirect("/urls");
-  }
-  req.session = null;
-  const templateVar = { user: undefined };
-  res.render("urls_login", templateVar);
-});
-
+// This endpoint registers a new url
 app.post("/urls", (req, res) => {
   const userId = req.session.user_id;
   if (!userId) {
@@ -104,9 +145,11 @@ app.post("/urls", (req, res) => {
   return res.redirect(`/urls/${id}`);
 });
 
+// This endpoint gets the edit url page
 app.get("/urls/:id", (req, res) => {
   const userId = req.session.user_id;
   if (!userId) {
+    res.status(401);
     return res.send("Error : 401, user not looged in");
   }
   if (!users[userId]) {
@@ -133,15 +176,29 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVar);
 });
 
-app.get("/u/:id", (req, res) => {
+// This endpoint edits/updates the url
+app.post("/urls/:id/update", (req, res) => {
+  const userId = req.session.user_id;
+  if (!userId) {
+    res.status(401);
+    return res.send("Error : 401, user not looged in");
+  }
+  if (!users[userId]) {
+    req.session = null;
+    res.status(404);
+    return res.send("Error: 404; User not found!");
+  }
   const id = req.params.id;
   if (!urlDatabase[id]) {
     res.status(404);
     return res.send("Error : 404, id not found");
   }
-  res.redirect(urlDatabase[id].longURL);
+  const newURL = req.body["New URL"];
+  urlDatabase[id].longURL = newURL;
+  res.redirect("/urls");
 });
 
+//This endpoint deletes the url
 app.post("/urls/:id/delete", (req, res) => {
   const userId = req.session.user_id;
   if (!userId) {
@@ -167,98 +224,17 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post("/urls/:id/edit", (req, res) => {
-  const userId = req.session.user_id;
-  if (!userId) {
-    res.status(401);
-    return res.send("Error : 401, user not looged in");
-  }
-  if (!users[userId]) {
-    req.session = null;
-    res.status(404);
-    return res.send("Error: 404; User not found!");
-  }
+// This endpoint redirects to long URL
+app.get("/u/:id", (req, res) => {
   const id = req.params.id;
   if (!urlDatabase[id]) {
     res.status(404);
     return res.send("Error : 404, id not found");
   }
-  const userUrlData = urlsForUser(urlDatabase, userId);
-  if (!userUrlData[id]) {
-    res.status(403);
-    return res.send("Error: 403, access denied");
-  }
-  const user = users[userId];
-  const templateVar = {
-    user: user,
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id].longURL,
-  };
-  res.render("urls_show", templateVar);
+  res.redirect(urlDatabase[id].longURL);
 });
 
-app.post("/urls/:id/update", (req, res) => {
-  const userId = req.session.user_id;
-  if (!userId) {
-    res.status(401);
-    return res.send("Error : 401, user not looged in");
-  }
-  if (!users[userId]) {
-    req.session = null;
-    res.status(404);
-    return res.send("Error: 404; User not found!");
-  }
-  const id = req.params.id;
-  if (!urlDatabase[id]) {
-    res.status(404);
-    return res.send("Error : 404, id not found");
-  }
-  const newURL = req.body["New URL"];
-  urlDatabase[id].longURL = newURL;
-  res.redirect("/urls");
-});
-
-app.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  if (!getUserByEmail(users, email)) {
-    res.status(403);
-    return res.send("Error: 403; email not found");
-  }
-  const id = getUserByEmail(users, email);
-  const userPassword = users[id].hashedPassword;
-  if (!bcrypt.compareSync(password, userPassword)) {
-    res.status(403);
-    return res.send("Error: 403; password does not match");
-  }
-  req.session.user_id = id; // eslint-disable-line camelcase
-  res.redirect("/urls");
-});
-
-app.post("/logout", (req, res) => {
-  req.session = null;
-  res.redirect("/login");
-});
-
-app.post("/register", (req, res) => {
-  const id = generateRandomString();
-  const email = req.body.email;
-  const password = req.body.password;
-  const hashedPassword = bcrypt.hashSync(password, 10);
-
-  if (!email || !password) {
-    res.status(400);
-    return res.send("Error:400; Invalid email or password");
-  }
-  if (getUserByEmail(users, email)) {
-    res.status(400);
-    return res.send("Error:400; user already exists");
-  }
-  users[id] = { id, email, hashedPassword };
-  req.session.user_id = id; // eslint-disable-line camelcase
-  res.redirect("/urls");
-});
-
+// Starting express server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
